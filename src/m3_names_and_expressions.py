f6793052c3569184c0632214d0f694ae6c79bd2a
watt <watt@rose-hulman.edu>
2019-01-17 13:16:10 -0500

    working

--- src/m3_names_and_expressions.py
+++ src/m3_names_and_expressions.py
@@ -3,8 +3,8 @@ answer = 2 ** 5
 print(answer * 100)
 
 ###############################################################################
-# TODO: 1.
-#   Read the 2 lines of code ABOVE this _TODO_.  That code:
+# DONE: 1.
+#   Read the 2 lines of code ABOVE this _TODO_.
 #     1. Computes 2 raised to the 5th power,
 #          yielding the object that is the integer 32.
 #     2. Makes the name   answer   refer to that object.
@@ -20,7 +20,7 @@ print(answer * 100)
 ###############################################################################
 
 ###############################################################################
-# TODO: 2.
+# DONE: 2.
 #   Immediately below this _TODO_, write code that:
 #     - Computes 77 plus the cosine of 2.75.
 #         HINT: You will need to import the   math  module (library).
@@ -28,7 +28,10 @@ print(answer * 100)
 #     - Prints the square root of that computed value.
 #   Run your code (fix errors as needed), then change the above _TODO_ to DONE.
 ###############################################################################
-
+import math
+answer = 77 + math.cos(2.75)
+answer1=math.sqrt(answer)
+print(answer1)
 ###############################################################################
 # TODO: 3.
 #   Immediately below this _TODO_, write code that computes and prints:
